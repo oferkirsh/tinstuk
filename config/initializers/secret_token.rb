6d2b0fa1deb6a317e601cc22f0972293a8c19177
oferkirsh <oferkirsh@gmail.com>
2015-07-06 21:59:01 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tinstuk::Application.config.secret_key_base = 'f86822d91cb295351000a47c5e151963a3ae4b7e4963277a4ed3bcdac55d579052128b12afb36b1057fa835a22fbada07977909d077d32141253c0691c558bbe'
